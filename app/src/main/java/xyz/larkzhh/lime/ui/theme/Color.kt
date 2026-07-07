906d2fe0ffa16277165c425826a2931d775b0a79
larkz-hh <3632378642@qq.com>
2026-07-07 22:41:50 +0800

    feat: 添加和修改颜色与主题

--- app/src/main/java/xyz/larkzhh/lime/ui/theme/Color.kt
+++ app/src/main/java/xyz/larkzhh/lime/ui/theme/Color.kt
@@ -1,11 +1,14 @@
 package xyz.larkzhh.lime.ui.theme
 
 import androidx.compose.ui.graphics.Color
+// Light theme
+val LimePrimary = Color(0xFF4A9B6F)
+val LimePrimaryLight = Color(0xFF6DB890)
+val LimePrimaryPale = Color(0xFFE8F5EE)
+val LimeGray = Color(0xFF888888)
+val LimeLightGray = Color(0xFFF5F5F5)
+val LimeDark = Color(0xFF1A1A1A)
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Dark theme
+val LimePrimaryDark = Color(0xFF6DB890)
+val LimeSurface = Color(0xFF2C2C2C)
