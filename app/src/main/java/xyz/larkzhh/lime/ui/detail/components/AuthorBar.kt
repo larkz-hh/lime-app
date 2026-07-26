6b2e9cfce5b772d21bed7c4046d86c9bdd57c2b5
larkz-hh <3632378642@qq.com>
2026-07-26 21:22:42 +0800

    feat(detail): 顶部头像裁剪适应圆形形状

--- app/src/main/java/xyz/larkzhh/lime/ui/detail/components/AuthorBar.kt
+++ app/src/main/java/xyz/larkzhh/lime/ui/detail/components/AuthorBar.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -57,6 +58,7 @@ fun AuthorBar(
         AsyncImage(
             model = note.author.avatar,
             contentDescription = null,
+            contentScale = ContentScale.Crop,
             modifier = Modifier
                 .size(38.dp)
                 .clip(CircleShape)
